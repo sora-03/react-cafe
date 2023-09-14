1e6530dd894b273cd486ae161a4c33bff5c0b913
sora <sora1003one@gmail.com>
2023-09-14 22:55:16 +0900

    header update

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,6 +7,25 @@ const Header = () => {
       <p className="flex justify-center text-yellow-300">
         緑豊かななく浮かんで心満ちる時間を
       </p>
+      <nav className="text-center mt-4">
+        <ul className="m-0 p-0">
+          <li className="inline-block w-24 hover:border-b-2 hover:border-yellow-300">
+            <a href="#" className="text-green-900">HOME</a>
+          </li>
+          <li className="inline-block w-24 hover:border-b-2 hover:border-yellow-300">
+            <a href="#" className="text-green-900">お知らせ</a>
+          </li>
+          <li className="inline-block w-24 hover:border-b-2 hover:border-yellow-300">
+            <a href="#" className="text-green-900">メニュー</a>
+          </li>
+          <li className="inline-block w-24 hover:border-b-2 hover:border-yellow-300">
+            <a href="#" className="text-green-900">カレンダー</a>
+          </li>
+          <li className="inline-block w-24 hover:border-b-2 hover:border-yellow-300">
+            <a href="#" className="text-green-900">店舗情報</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
